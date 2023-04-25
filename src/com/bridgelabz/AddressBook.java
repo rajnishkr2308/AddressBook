1251983ff9667b4d69a7cea749dd4b1a1c665f65
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-04-25 16:42:07 +0530

    Added:Contact created in address Book

--- src/com/bridgelabz/AddressBook.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz;
-
-public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-    }
-
-}
---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+
+        Contact contacts = new Contact("rajnish","kumar","neargurukripa","Biharsharif","Bihar","803107","7541061533","rajneeshsingh9905@gmail.com")
+                ;
+        System.out.println(contacts);
+    }
+}
